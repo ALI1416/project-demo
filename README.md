3218820342324ef801adce29d5a59ea5dd881c28
ALI <1416978277@qq.com>
2023-11-19 21:28:47 +0800

    1119

---
+++ README.md
@@ -0,0 +1,13 @@
+# project-demo 项目示例
+
+[![License](https://img.shields.io/github/license/ALI1416/project-demo?label=License)](https://opensource.org/licenses/BSD-3-Clause)
+[![Java Support](https://img.shields.io/badge/Java-8+-green)](https://openjdk.org/)
+[![Repo Size](https://img.shields.io/github/repo-size/ALI1416/project-demo?label=Repo%20Size&color=success)](https://github.com/ALI1416/project-demo/archive/refs/heads/master.zip)
+
+## 简介
+
+项目示例
+
+## 项目
+
+- `demo-ws` WebSocket示例
---
+++ demo-ws/src/main/java/com/demo/controller/WsController.java
@@ -0,0 +1,52 @@
+package com.demo.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+
+/**
+ * <h1>WsController</h1>
+ *
+ * <p>
+ * createDate 2021/12/16 10:05:53
+ * </p>
+ *
+ * @author ALI[ali-k@foxmail.com]
+ * @since 1.0.0
+ **/
+@Controller
+@AllArgsConstructor
+@Slf4j
+public class WsController {
+
+    /**
+     * WebSocket模板
+     */
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    /**
+     * 广播模式
+     */
+    @MessageMapping("/broadcast")
+    public String broadcast(String msg, Principal principal) {
+        String data = "用户[" + principal.getName() + "]发送广播消息[" + msg + "]";
+        log.info(data);
+        return data;
+    }
+
+    /**
+     * 用户模式
+     */
+    @MessageMapping("/sendToUser/{user}")
+    public void sendToUser(@DestinationVariable String user, String msg, Principal principal) {
+        String data = "用户[" + principal.getName() + "]发送给用户[" + user + "]消息[" + msg + "]";
+        log.info(data);
+        simpMessagingTemplate.convertAndSendToUser(user, "/queue/sendToUser", data);
+    }
+
+}
