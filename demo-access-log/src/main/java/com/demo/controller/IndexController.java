b44cb4c56cb8dce96c99083edeea4097a9a15151
ALI <1416978277@qq.com>
2023-11-25 16:37:16 +0800

    1125

--- demo-access-log/src/main/java/com/demo/controller/IndexController.java
+++ demo-access-log/src/main/java/com/demo/controller/IndexController.java
@@ -1,12 +1,13 @@
 package com.demo.controller;
 
 import com.demo.entity.po.LoginLog;
-import com.demo.entity.pojo.Result;
 import com.demo.service.LoginLogService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -32,10 +33,11 @@ public class IndexController implements ErrorController {
      * 页面找不到
      */
     @RequestMapping("error")
-    public Result<LoginLog> error(HttpServletRequest request) {
+    @ResponseStatus(HttpStatus.OK)
+    public LoginLog error(HttpServletRequest request) {
         LoginLog loginLog = loginLogService.insert(request);
         log.info(loginLog.toString());
-        return Result.o(loginLog);
+        return loginLog;
     }
 
 }
